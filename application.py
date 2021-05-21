d15b93e781dfa2c093db415d498b5d7d91c76a60
Tim Mok <timothyjmok@gmail.com>
2021-05-21 18:03:11 -0400

    completed models and migrations

--- application.py
+++ application.py
@@ -6,10 +6,27 @@ import jwt
 import sqlalchemy
 app = Flask(__name__)
 CORS(app)
-
 bcrypt = Bcrypt(app)
-
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 import models
 models.db.init_app(app)
+
+
+def root():
+    return 'May the force be with you.'
+
+app.route('/', methods=["GET"])(root)
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
